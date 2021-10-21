4c42683295e05480b1cfbb5c6862f411d0f4acf2
tommasobellini <deev@tommasobellini.it>
2021-10-21 22:25:38 +0200

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI, File
+from pydantic import BaseModel
+from datetime import datetime, time, timedelta
+
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+class CountDownSchema(BaseModel):
+    name: str
+    start_date: datetime
+    end_date: datetime
+
+
+@app.get("/get_countdown/")
+def get_countdown():
+    return {
+        "name": "First countdown of the year",
+        "start_date": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+        "end_date": (datetime.now() + timedelta(hours=2)).strftime("%d-%m-%Y %H:%M:%S"),
+    }
+
+
+@app.post("/create_countdown/")
+def create_countdown(body: CountDownSchema):
+    return {
+        "name": "First countdown of the year",
+        "start_date": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+        "end_date": (datetime.now() + timedelta(hours=2)).strftime("%d-%m-%Y %H:%M:%S"),
+    }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
