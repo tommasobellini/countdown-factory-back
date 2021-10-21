c853f3c20be2907be627b0f45bc7c904e65f9f2e
tommasobellini <deev@tommasobellini.it>
2021-10-21 22:37:25 +0200

    fix

--- main.py
+++ main.py
@@ -43,10 +43,5 @@ def create_countdown(body: CountDownSchema):
     }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
